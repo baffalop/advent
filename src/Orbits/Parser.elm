08b6a196c53cfcacb8470f551573d25a295a4a05
Nikita Gaidakov <ngaidakov@podfather.com>
2019-12-21 14:43:10 +0100

    Write parser for map syntax

--- src/Orbits/Parser.elm
+++ src/Orbits/Parser.elm
@@ -1,3 +1,60 @@
-module Orbits.Parser exposing (..)
+module Orbits.Parser exposing (map, parse)
 
-import Parser as P
+import Parser
+    exposing
+        ( (|.)
+        , (|=)
+        , Parser
+        , Trailing(..)
+        , chompWhile
+        , sequence
+        , succeed
+        , symbol
+        , variable
+        )
+import Set
+
+
+type alias Mapping =
+    ( String, String )
+
+
+body : Parser String
+body =
+    variable
+        { start = Char.isAlpha
+        , inner = Char.isAlphaNum
+        , reserved = Set.empty
+        }
+
+
+lineSpaces : Parser ()
+lineSpaces =
+    chompWhile (\c -> c == ' ' || c == '\t')
+
+
+orbit : Parser Mapping
+orbit =
+    succeed Tuple.pair
+        |= body
+        |. lineSpaces
+        |. symbol ")"
+        |. lineSpaces
+        |= body
+
+
+map : Parser (List Mapping)
+map =
+    sequence
+        { start = ""
+        , end = ""
+        , separator = "\n"
+        , spaces = lineSpaces
+        , item = orbit
+        , trailing = Optional
+        }
+
+
+parse : String -> Result (List Parser.DeadEnd) (List Mapping)
+parse =
+    Parser.run map
