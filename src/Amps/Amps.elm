00870bd7a2388e265a0029aa0a081dbe4f4ce443
Nikita Gaidakov <ngaidakov@podfather.com>
2019-12-23 11:44:32 +0100

    Solve 7.1!

--- src/Amps/Amps.elm
+++ src/Amps/Amps.elm
@@ -1,6 +1,8 @@
-module Amps.Amps exposing (runAmps)
+module Amps.Amps exposing (optimiseAmps, runAmps)
 
 import Intcodes.Intcodes as Intcodes exposing (OpResult(..))
+import Set
+import Utils exposing (filterMaybes)
 
 
 toResult : OpResult -> Result String (List Int)
@@ -23,3 +25,40 @@ runAmps program phases =
                 lastResult
     in
     List.foldl runAmp (Ok [ 0 ]) phases
+
+
+permuteUnique : Int -> List (List Int)
+permuteUnique limit =
+    let
+        seed =
+            List.repeat limit (List.range 0 (limit - 1))
+
+        addToPermutations add perms =
+            List.concatMap
+                (\n ->
+                    List.filter (not << List.member n) perms
+                        |> List.map ((::) n)
+                )
+                add
+    in
+    List.foldl addToPermutations (List.repeat limit []) seed
+        |> Set.fromList
+        |> Set.toList
+
+
+optimiseAmps : List Int -> Int -> Maybe ( List Int, List Int )
+optimiseAmps program count =
+    let
+        resultsToMaybe ( result, phases ) =
+            case result of
+                Err _ ->
+                    Nothing
+
+                Ok x ->
+                    Just ( x, phases )
+    in
+    List.map (\phases -> ( runAmps program phases, phases )) (permuteUnique count)
+        |> List.map resultsToMaybe
+        |> filterMaybes
+        |> List.sortBy (Tuple.first >> List.head >> Maybe.withDefault 0 >> negate)
+        |> List.head
