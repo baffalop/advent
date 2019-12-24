6ef1490b73d939a76bc4b77bc42d1b519974d440
Nikita Gaidakov <ngaidakov@podfather.com>
2019-12-24 11:48:23 +0100

    Solved 8.2!

--- src/Image/Solution.elm
+++ src/Image/Solution.elm
@@ -1,8 +1,12 @@
-module Image.Solution exposing (checksum, partitionImage)
+module Image.Solution exposing (checksum, partitionImage, renderImage)
+
+
+type alias Layer =
+    List (List Int)
 
 
 type alias Image =
-    List (List (List Int))
+    List Layer
 
 
 chunk : Int -> List a -> List (List a)
@@ -36,3 +40,66 @@ checksum image =
             )
         |> List.sortBy Tuple.first
         |> List.head
+
+
+overlayPixels : Int -> Int -> Int
+overlayPixels bottom top =
+    if top < 2 then
+        top
+
+    else
+        bottom
+
+
+detectDimensions : Image -> ( Int, Int )
+detectDimensions image =
+    let
+        layer =
+            List.head image
+
+        row =
+            Maybe.andThen List.head layer
+
+        length =
+            Maybe.map List.length >> Maybe.withDefault 0
+    in
+    ( length row, length layer )
+
+
+overlayLayers : Image -> Layer
+overlayLayers image =
+    let
+        ( width, height ) =
+            detectDimensions image
+
+        emptyLayer =
+            List.repeat width 2
+                |> List.repeat height
+    in
+    List.foldl
+        (List.map2 (List.map2 overlayPixels))
+        emptyLayer
+        image
+
+
+renderPixel : Int -> Char
+renderPixel pixel =
+    case pixel of
+        1 ->
+            '#'
+
+        _ ->
+            ' '
+
+
+renderLayer : Layer -> String
+renderLayer layer =
+    List.map (List.map renderPixel) layer
+        |> List.intersperse [ '\n' ]
+        |> List.concat
+        |> String.fromList
+
+
+renderImage : Image -> String
+renderImage =
+    overlayLayers >> renderLayer
