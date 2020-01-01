b59f6295fa73a6d42c99e6848ce581cadda89e35
Nikita Gaidakov <ngaidakov@podfather.com>
2020-01-01 20:56:45 +0000

    Start on some types and parsing

--- src/Arcade/Game.elm
+++ src/Arcade/Game.elm
@@ -1,1 +1,54 @@
 module Arcade.Game exposing (..)
+
+import Intcodes.Intcodes as IC exposing (OpResult(..))
+
+
+type alias Tile =
+    { loc : ( Int, Int )
+    , tileType : TileType
+    }
+
+
+type TileType
+    = Unrecognised
+    | Empty
+    | Wall
+    | Block
+    | Paddle
+    | Ball
+
+
+parseOutput : List Int -> ( List Tile, List Int )
+parseOutput output =
+    case output of
+        x :: y :: tile :: rest ->
+            let
+                ( tiles, leftovers ) =
+                    parseOutput rest
+            in
+            ( { loc = ( x, y ), tileType = parseTile tile } :: tiles, leftovers )
+
+        leftovers ->
+            ( [], leftovers )
+
+
+parseTile : Int -> TileType
+parseTile input =
+    case input of
+        0 ->
+            Empty
+
+        1 ->
+            Wall
+
+        2 ->
+            Block
+
+        3 ->
+            Paddle
+
+        4 ->
+            Ball
+
+        _ ->
+            Unrecognised
