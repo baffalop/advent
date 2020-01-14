8ac39de2e89ef211b9be83baee9067aef3053511
Nikita Gaidakov <ngaidakov@podfather.com>
2020-01-14 23:09:46 +0000

    Towards a solution, but atomic handling of prices leads to overcounting

--- src/Factory/Factory.elm
+++ src/Factory/Factory.elm
@@ -3,14 +3,65 @@ module Factory.Factory exposing (..)
 import Dict exposing (Dict)
 import Parser exposing ((|.), (|=), Parser)
 import Set
+import Utils exposing (flip)
 
 
 type alias Price =
-    ( Int, String )
+    ( String, Int )
 
 
 type alias Reactions =
     Dict String ( Int, List Price )
+
+
+expandPrice : Reactions -> Price -> Maybe (List Price)
+expandPrice reactions ( name, requiredQty ) =
+    Dict.get name reactions
+        |> Maybe.map
+            (\( acquiredQty, priceList ) ->
+                let
+                    multiple =
+                        ceiling (toFloat requiredQty / toFloat acquiredQty)
+                in
+                if multiple == 1 then
+                    priceList
+
+                else
+                    List.map (Tuple.mapSecond ((*) multiple)) priceList
+            )
+
+
+expandPrices : Reactions -> List Price -> Maybe (List Price)
+expandPrices reactions prices =
+    case prices of
+        [] ->
+            Just []
+
+        price :: rest ->
+            if Tuple.first price == "ORE" then
+                Maybe.map ((::) price) (expandPrices reactions rest)
+
+            else
+                Maybe.map (flip (++) rest >> consolidatePrices) (expandPrice reactions price)
+                    |> Maybe.andThen (expandPrices reactions)
+
+
+consolidatePrices : List Price -> List Price
+consolidatePrices prices =
+    List.foldl
+        (\( name, qty ) ledger ->
+            Dict.update name (\x -> Maybe.withDefault 0 x + qty |> Just) ledger
+        )
+        Dict.empty
+        prices
+        |> Dict.toList
+
+
+findPriceOfFuel : Reactions -> Maybe Int
+findPriceOfFuel reactions =
+    expandPrice reactions ( "FUEL", 1 )
+        |> Maybe.andThen (expandPrices reactions)
+        |> Maybe.map (List.map Tuple.second >> List.sum)
 
 
 
@@ -19,7 +70,7 @@ type alias Reactions =
 
 priceParser : Parser Price
 priceParser =
-    Parser.succeed Tuple.pair
+    Parser.succeed (flip Tuple.pair)
         |= Parser.int
         |. Parser.spaces
         |= Parser.variable
@@ -76,3 +127,4 @@ reactionsParser =
 parse : String -> Reactions
 parse =
     Parser.run reactionsParser
+        >> Result.withDefault Dict.empty
